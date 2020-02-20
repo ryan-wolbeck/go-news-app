ed73e8c7d47a0ba3dd312fd103cb7ab77c589c98
Ryan <ryan.wolbeck@chrobinson.com>
2020-02-20 10:01:04 -0600

    parse the xml

--- main.go
+++ main.go
@@ -2,12 +2,26 @@ package main
 
 import ("fmt"
 		"net/http"
-		"io/ioutil")
+		"io/ioutil"
+		"encoding/xml")
+
+type SitemapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
 
 func main() {
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	string_body := string(bytes)
-	fmt.Println(string_body)
 	resp.Body.Close()
+	var s  SitemapIndex
+	xml.Unmarshal(bytes, &s)
+	fmt.Println(s.Locations)
 }
