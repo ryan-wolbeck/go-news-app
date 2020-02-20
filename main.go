d0bedd58ba67965409c26dc5ce15479e1ea5dd1e
Ryan <ryan.wolbeck@chrobinson.com>
2020-02-20 10:42:19 -0600

    clean up formatting

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
-import ("fmt"
-		"net/http"
-		"io/ioutil"
-		"encoding/xml")
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 type SitemapIndex struct {
 	Locations []Location `xml:"sitemap"`
@@ -21,7 +23,7 @@ func main() {
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
-	var s  SitemapIndex
+	var s SitemapIndex
 	xml.Unmarshal(bytes, &s)
 	//fmt.Println(s.Locations)
 	for _, Location := range s.Locations {
